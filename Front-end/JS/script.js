8f2d55e3f5daf9cf611c20e13e2ccdbba83cc997
11999977 <boukoussoudahel@gmail.com>
2024-02-07 23:36:05 +0100

    Mon portofolio première version

---
+++ Front-end/JS/script.js
@@ -0,0 +1,17 @@
+// document.getElementById('btnDownload').addEventListener('click', function() {
+//   // Code pour télécharger le PDF
+// });
+
+
+// document.getElementById('btnDownload').addEventListener('click', function() {
+//     // Créer un lien temporaire vers le fichier PDF
+//     let link = document.createElement('a');
+//     link.href = '../src/CV-Aron.pdf';
+//     link.download = 'CV-Aron.pdf';
+//     document.body.appendChild(link);
+//     // Simuler un clic sur le lien pour déclencher le téléchargement
+//     link.click();
+//     // Nettoyer après le téléchargement
+//     document.body.removeChild(link);
+// });
+
